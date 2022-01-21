3bfc61febbfd3d5729232395f860534bcf865a75
Excellence KAWEJ <19ka201@esisalama.org>
2022-01-21 10:17:29 +0200

    vérification, récupération et affichage des données

---
+++ controllers/controller.php
@@ -0,0 +1,15 @@
+<?php
+    if (isset($_POST)){
+        $name = $_POST['name'];
+        $lastname = $_POST['lastname'];
+        $firstname = $_POST['firstname'];
+        $matricule = $_POST['matricule'];
+        $gender = $_POST['gender'];
+        $promotion = $_POST['promotion'];
+        $email = $_POST['email'];
+        echo "Nom : $name <br> Postnom : $lastname <br> Prenom : $firstname <br> Matricule : $matricule <br> Genre : $gender <br> Promotion : $promotion <br> Email : $email";
+    } else{
+        echo "Désolé ce tableau n'existe pas";
+    }
+
+?>
---
+++ index.php
@@ -0,0 +1,24 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="controllers/controller.php" method="post">
+        Nom : <input type="text" name="name" placeholder="Entrer le nom"><br>
+        Postnom : <input type="text" name="lastname" placeholder="Entrer le postnom"><br>
+        Prenom : <input type="text" name="firstname" placeholder="Entrer le prénom"><br>
+        Matricule : <input type="text" name="matricule" placeholder="Entrer le matricule"><br>
+        Genre : 
+        <select name="gender" id="">
+            <option value="M">Masculin</option>
+            <option value="F">Feminin</option>
+        </select name="gender"><br>
+        Promotion : <input type="text" name="promotion" placeholder="Entrer la promotion"><br>
+        Email : <input type="email" name="email"placeholder="Entrer l'addresse mail"><br>
+        <input type="submit" value="submit">
+    </form>
+</body>
+</html>
