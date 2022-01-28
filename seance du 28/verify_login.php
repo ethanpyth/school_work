a1bc2ab7cd86449d7a4f345afe5a591707f04648
Excellence KAWEJ <19ka201@esisalama.org>
2022-01-28 10:15:06 +0200

    redirection vers la page de connexion

--- seance du 28/verify_login.php
+++ seance du 28/verify_login.php
@@ -1,3 +1,13 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
     session_start(); 
     $donnees = array('nom' => $_POST['nom'],'email' => 'excellencekawej@gmail.com', 'passwd'=>'ethan0901');
@@ -5,7 +15,12 @@
         $_SESSION['nom'] = $_POST['nom'];
         $_SESSION['email'] = $_POST['email'];
         echo '<p>Bonjour ' . $_SESSION['nom'] . ' votre mail est ' . $_SESSION['email'] . ' et bienvenu.</p>';
+    }else{
+        echo '<p>Veuillez remplir tous les champs</p>';
     }
-    $list = array();
+    
     session_destroy();
-?>
+    ?>
+    <button><a href="login.php">Revenir à la page de connexion</a></button> 
+</body>
+</html>
